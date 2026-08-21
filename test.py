fbcea3c441b2f8eca1285d457db398044d8c2439
Patel Kamlesh Rameshbhai <kamlesh0408@hotmail.com>
2026-08-21 18:13:44 +0530

    App code Added.

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
+#from backend import get_llm
 
 
-##res = tavily_search("Best hotels in India")
 
-res = search_flights("Plan a 7 days Nepal trip from Bangladesh")
-print(res)
+
+# res = search_flights("Plan a 7 days Nepal trip from Bangladesh")
+# print(res)
+
+input_msg = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=input_msg,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
